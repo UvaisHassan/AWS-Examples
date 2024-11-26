e520bfaffbd171fcd81b2a3da189b682ff82d0f9
uvaishassan <uvaishassan@gmail.com>
2024-11-26 10:57:20 +0000

    Track S3 object change with etag

---
+++ s3/etags/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.77.0"
+    }
+  }
+}
+
+provider "aws" {
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "hello_world.txt"
+  source = "hello_world.txt"
+
+  etag = filemd5("hello_world.txt")
+}
