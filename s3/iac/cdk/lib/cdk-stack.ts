00c9c744f88dd51c0fda9e10416f290d96ff6f90
uvaishassan <uvaishassan@gmail.com>
2024-11-26 04:37:40 +0000

    CDK for S3

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    new s3.Bucket(this, "my-example-bucket-uvais-cdk");
+  }
+}
